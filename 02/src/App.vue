29bde7094a233076a3fe8d6e418efa6aeffd364a
kelvingao <89156201@qq.com>
2019-03-16 01:01:58 +0800

    Targeting Classes and ID's

---
+++ 02/src/App.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  #app
+  
+    #header
+      h1 Contact Us
+      h2 Contace The Net Ninja
+
+    h2#content-header content
+
+    p Hello there ninjas! contract us below.
+    p Aren't paragraph's cool?
+
+    a(href="http://www.google.com") Google
+
+    span Yo, I'm a span tag
+
+    span.deck Yo, I'm a span tag too
+    span.deck This is a deck of spans
+    span.deck I like span decks
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+ 
+}
+</script>
+
+<style>
+h2 {
+  font-size: 24px;
+}
+#content-header {
+  font-size: 18px;
+  color: red;
+}
+
+span {
+  display: block;
+  color: blue;
+}
+
+.deck {
+  color: red;
+}
+</style>
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# css_tutorial
+# Classes & ID's
+
+## Both are a way to describe your content
+
+- Classes can be used multiple times on a page
+- ID's can be used only once per page - they are unique
