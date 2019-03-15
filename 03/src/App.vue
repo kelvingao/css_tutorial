69a4adac4a8b1ae0700e6bdcd4a81f70a621f053
kelvingao <89156201@qq.com>
2019-03-16 01:01:58 +0800

    Selector Specificty

---
+++ 03/src/App.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  #app
+  
+    #header
+      h1 Contact Us
+      h2 Contace The Net Ninja
+
+    h2#content-header content
+
+    #main-content
+      p Hello there #[strong ninjas!] contract us below.
+      p.test Aren't paragraph's cool?
+
+    a(href="http://www.google.com") Google
+
+    span Yo, I'm a span tag
+
+    span.deck Yo, I'm a span tag too
+    span.deck This is a deck of spans
+    span.deck I like span decks
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+ 
+}
+</script>
+
+<style>
+#main-content p{
+  color: red; /* has 101 points */
+}
+p {
+  color: black; /* only 1 point */
+}
+.test {
+  color: green; /* class has 10 points */
+}
+strong {
+  color: green; /* only inheriting #main-content p, override this */
+}
+</style>
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# css_tutorial
+# Selector Specificty
+
+## Confilicts & Cascade
+```CSS
+#main-content p { color: black; }   - This rule wins
+
+p { color: red; }
+```
+
+## How Specific ?
+>if selectors are identical, which selector has more points wins
+* ID's 100 points
+* Classes 10 points
+* Elements 1 point
